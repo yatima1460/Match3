be02f623707d140d49a99fe2143bda0a2dcef298
Federico Santamorena <federico@santamorena.me>
2020-04-06 03:50:20 +0200

    remove headless test

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -10,11 +10,6 @@ int main(int argc, char const *argv[])
     // Set random seed for gems spawning
     srand((unsigned int)time(NULL));
 
-    const char *headless = "--headless";
-    const char *headless_command = "SDL_VIDEODRIVER=dummy";
-    if (argc == 2 && strcmp(argv[1], headless) == 0)
-        _putenv((char *)headless_command);
-
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Game starting...");
     Game::Start();
     return EXIT_SUCCESS;
